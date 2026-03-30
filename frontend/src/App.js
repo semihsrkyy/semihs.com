5a47c6c29a8715b56cc4e104ef0b4e1c8bf8d6ab
semihsrkyy <sariakayasemih@gmail.com>
2026-03-30 15:02:40 +0300

    Create React dashboard component for monitoring visa appointments from VFS Global.

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Visa Appointment Monitoring Dashboard</h1>
+            <p>This component is designed to monitor visa appointments from VFS Global.</p>
+            {/* Additional dashboard functionality will be added here */}
+        </div>
+    );
+};
+
+export default Dashboard;
